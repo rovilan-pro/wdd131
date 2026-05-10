822b623533612c440eb8b487fccc544b187d6af1
rovilan-pro <rovilec143@gmail.com>
2026-05-10 23:55:52 +0800

    Week01

---
+++ week01/scripts/getdates.js
@@ -0,0 +1,5 @@
+document.getElementById("currentyear").textContent =
+new Date().getFullYear();
+
+document.getElementById("lastModified").textContent =
+`Last Modification: ${document.lastModified}`;
