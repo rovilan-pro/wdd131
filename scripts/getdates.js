5a86e29ad3a95a3c830545613e7cdcb5f9f9049f
rovilan-pro <rovilec143@gmail.com>
2026-05-17 23:48:38 +0800

    Testing 3

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,3 +3,16 @@ new Date().getFullYear();
 
 document.getElementById("lastModified").textContent =
 `Last Modification: ${document.lastModified}`;
+
+const menuButton = document.querySelector("#menu-button");
+const nav = document.querySelector("nav");
+
+menuButton.addEventListener("click", () => {
+    nav.classList.toggle("open");
+
+    if (nav.classList.contains("open")) {
+        menuButton.textContent = "✖";
+    } else {
+        menuButton.textContent = "☰";
+    }
+});
